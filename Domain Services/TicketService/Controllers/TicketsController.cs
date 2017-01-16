1a2c76ecf4c0ca5912fb1f0011d64ac9090d1b3e
Konrad Kokosa <kkokosa@f28-studio.com>
2017-01-17 00:37:37 +0100

    Second service added

--- Domain Services/TicketService/Controllers/TicketsController.cs
+++ Domain Services/TicketService/Controllers/TicketsController.cs
@@ -7,35 +7,35 @@ using Microsoft.AspNetCore.Mvc;
 namespace TicketService.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TicketsController : Controller
     {
-        // GET api/values
+        // GET api/tickets
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/tickets/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/tickets
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/tickets/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/tickets/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
