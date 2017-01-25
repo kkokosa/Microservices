b0e659ff104fa9f4e81e52b9f69ba37f6ed5287a
Konrad Kokosa <kkokosa@f28-studio.com>
2017-01-25 17:57:48 +0100

    Command and queries for Ticket Service. DDD framework initial commit.

---
+++ Domain Services/TicketService/TicketService.Domain/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TicketService.Domain")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e0bd36d7-ea28-4d3e-ac06-2c78b8ad563c")]
---
+++ Domain Services/TicketService/TicketService.Domain/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using ServicesFramework.DDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicketService.Domain
+{
+    public class Ticket : AggregateRoot
+    {
+    }
+}
