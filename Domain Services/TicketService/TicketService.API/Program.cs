6825384f0ed962d908e81f5f67ab8a3ffdf572c9
Konrad Kokosa <kkokosa@f28-studio.com>
2018-01-14 12:29:16 +0100

    TicketService upgraded to ASP.NET Core 2.0

--- Domain Services/TicketService/TicketService.API/Program.cs
+++ Domain Services/TicketService/TicketService.API/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 
@@ -12,7 +13,7 @@ namespace TicketService
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
+            var host = WebHost.CreateDefaultBuilder(args)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
