ffbd04bab27a37a26f582479450f0095a719d207
kkokosa <kkokosa@f28-studio.com>
2018-01-16 18:24:14 +0000

    Connection string read from configuration

--- Domains/Trips/API/Trips.Queries/Handlers/GetAllOffersQueryHandler.cs
+++ Domains/Trips/API/Trips.Queries/Handlers/GetAllOffersQueryHandler.cs
@@ -5,15 +5,23 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using Dapper;
+using Microsoft.Extensions.Configuration;
 using ServicesFramework.CQRS;
 
 namespace Trips.Queries.Handlers
 {
     public class GetAllOffersQueryHandler : IQueryHandler<GetAllOffersQuery, GetAllOffersQueryResult>
     {
+        private readonly IConfiguration configuration;
+
+        public GetAllOffersQueryHandler(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
         public GetAllOffersQueryResult Handle(GetAllOffersQuery command)
         {
-            using (IDbConnection db = new SqlConnection("Server=DotNetTraining\\SQLEXPRESS;Database=TravelAgency.TripsDomainDatabase;Trusted_Connection=True;"))
+            using (IDbConnection db = new SqlConnection(configuration["ConnectionStrings:TripsDomainDatabase"]))
             {
                 var offers = db.Query<OfferViewModel>("Select Name, Description, NumberOfDays From Offers").ToList();
                 return new GetAllOffersQueryResult()
